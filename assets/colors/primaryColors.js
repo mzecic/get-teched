28dbeb5dd962ecb6357eb5af4871601f93f4e2d4
mzecic <matejzecic@gmail.com>
2023-03-21 09:46:20 -0400

    reduce transparency on header and navbar

--- assets/colors/primaryColors.js
+++ assets/colors/primaryColors.js
@@ -5,7 +5,7 @@ const colors = {
   grey: "rgb(50, 50, 50)",
   lighterGrey: "rgb(215, 215, 215)",
   backgroundDarkMode: "rgb(25, 25, 25)",
-  backgroundLightMode: "rgba(240, 240, 247, 0.90)",
+  backgroundLightMode: "rgba(240, 240, 247, 0.95)",
   primaryHighlight: "rgb(150, 150, 150)",
   secondaryHighlight: "rgb(115, 115, 115)",
   appDrawerHighlight: "rgb(70, 70, 70)",
